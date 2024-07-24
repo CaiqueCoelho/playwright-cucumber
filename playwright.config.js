24d061348f87818b3cd8db94211519567da81952
Caíque Coelho <caiquedpfc@gmail.com>
2024-07-24 17:06:35 -0300

    fixing yml GHA

--- playwright.config.js
+++ playwright.config.js
@@ -12,7 +12,7 @@ const { defineConfig, devices } = require('@playwright/test');
  */
 module.exports = defineConfig({
   testDir: './tests',
-  timeout: 30 * 1000, // 30 seconds
+  timeout: 31 * 1000, // 30 seconds
   expect: {
     /**
      * Maximum time expect() should wait for the condition to be met.
